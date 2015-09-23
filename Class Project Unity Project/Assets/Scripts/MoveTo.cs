901709e5e63d3e55000488d00fd9f03f431353b4
Brian Foster <brianisbrilliant@gmail.com>
2015-09-23 12:42:19 -0500

    Added a navmesh
    for the AI

---
+++ Class Project Unity Project/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,18 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+   
+public class MoveTo : MonoBehaviour {
+       
+    public Transform goal;
+    NavMeshAgent agent;
+    
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    void Update() {
+    	agent.destination = goal.position;
+    }
+}
