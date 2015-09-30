6a2c326758be0fe826aba95fe130a1243bb24acf
Brian Foster <brianisbrilliant@gmail.com>
2015-09-30 14:20:30 -0500

    Week 7 edits

---
+++ Class Project Unity Project/Assets/Scripts/SunRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunRotate : MonoBehaviour {
+
+	public int rotSpeed = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotSpeed * Time.deltaTime,0,0);
+	}
+}
